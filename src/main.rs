8fe869acbb8a48318008e98bf21f114252365ac7
Mike Moran <mike@houseofmoran.com>
2021-06-11 17:08:03 +0100

    generate 1000 random entries in a String -> bool HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::collections::HashMap;
+
+fn random_key(key_length: usize) -> String {
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let mut rng = rand::thread_rng();
+
+    (0..key_length)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect()
+}
+
+fn random_value() -> bool {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    rng.gen_bool(0.5)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let key_length: usize = 30;
+    let num_entries = 1000;
+
+    let mut plain: HashMap<String, bool> = HashMap::new();
+    (0..num_entries).for_each(|_| {
+        plain.insert(random_key(key_length),random_value());
+    });
+
+    println!("{:?}", plain);
 }
