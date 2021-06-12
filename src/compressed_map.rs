190e6488f76059c54e40f6aa6189d60d680c1f69
Mike Moran <mike@houseofmoran.com>
2021-06-12 19:25:30 +0100

    - extract compressed_map

---
+++ src/compressed_map.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+
+use bloomfilter::Bloom;
+use deepsize::{Context, DeepSizeOf};
+
+pub struct CompressedMap {
+    bloom: Bloom<String>,
+    direct: HashMap<String, bool>
+}
+
+impl CompressedMap {
+    fn get(&self, k: &String) -> Option<&bool> {
+        if self.direct.contains_key(k) {
+            self.direct.get(k)
+        }
+        else {
+            if self.bloom.check(k) {
+                Some(&true)
+            }
+            else {
+                Some(&false)
+            }
+        }
+    }
+}
+
+impl DeepSizeOf for CompressedMap {
+    fn deep_size_of_children(&self, context: &mut Context) -> usize
+    {
+        self.bloom.bitmap().deep_size_of_children(context)
+            + self.direct.deep_size_of_children(context)
+    }
+}
+
+pub fn compress(original: &HashMap<String, bool>) -> CompressedMap {
+    let bitmap_size = 1024 / 10;
+    let mut bloom : Bloom<String> = Bloom::new(bitmap_size, original.len());
+    for (key, value) in original {
+        if *value {
+            bloom.set(key);
+        }
+    }
+
+    let mut direct: HashMap<String, bool> = HashMap::new();
+    for (key, value) in original {
+        if *value != bloom.check(key) {
+            direct.insert(key.clone(), value.clone());
+        }
+    }
+
+    CompressedMap {
+        bloom, direct
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{random_key, random_value};
+    use crate::compressed_map::compress;
+
+    #[test]
+    fn same_output() {
+        let key_length: usize = 30;
+        let num_entries = 1000;
+
+        let mut original: HashMap<String, bool> = HashMap::new();
+        (0..num_entries).for_each(|_| {
+            original.insert(random_key(key_length),random_value());
+        });
+
+        let bloom_map = compress(&original);
+
+        for key in original.keys() {
+            assert_eq!(bloom_map.get(key), original.get(key));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
-use bloomfilter::Bloom;
-use deepsize::{DeepSizeOf, Context};
+
+use deepsize::{Context, DeepSizeOf};
+
+mod compressed_map;
 
 fn random_key(key_length: usize) -> String {
     use rand::Rng;
@@ -22,56 +24,6 @@ fn random_value() -> bool {
     rng.gen_bool(0.5)
 }
 
-struct BloomMap {
-    bloom: Bloom<String>,
-    direct: HashMap<String, bool>
-}
-
-impl BloomMap {
-    fn get(&self, k: &String) -> Option<&bool> {
-        if self.direct.contains_key(k) {
-            self.direct.get(k)
-        }
-        else {
-            if self.bloom.check(k) {
-                Some(&true)
-            }
-            else {
-                Some(&false)
-            }
-        }
-    }
-}
-
-impl DeepSizeOf for BloomMap {
-    fn deep_size_of_children(&self, context: &mut Context) -> usize
-    {
-        self.bloom.bitmap().deep_size_of_children(context)
-            + self.direct.deep_size_of_children(context)
-    }
-}
-
-fn compress(original: &HashMap<String, bool>) -> BloomMap {
-    let bitmap_size = 1024 / 10;
-    let mut bloom : Bloom<String> = Bloom::new(bitmap_size, original.len());
-    for (key, value) in original {
-        if *value {
-            bloom.set(key);
-        }
-    }
-
-    let mut direct: HashMap<String, bool> = HashMap::new();
-    for (key, value) in original {
-        if *value != bloom.check(key) {
-            direct.insert(key.clone(), value.clone());
-        }
-    }
-
-    BloomMap {
-        bloom, direct
-    }
-}
-
 fn main() {
     let key_length: usize = 30;
     let num_entries = 1000;
@@ -83,37 +35,14 @@ fn main() {
 
     println!("{:?}", plain);
 
-    let bloom_map = compress(&plain);
+    let compressed_map = compressed_map::compress(&plain);
 
     let plain_size = plain.deep_size_of();
-    let final_size = bloom_map.deep_size_of();
+    let final_size = compressed_map.deep_size_of();
     let saving_percent = 100.0 * (1.0 - (final_size as f32 / plain_size as f32));
 
     println!("plain size: {}, final total: {}, saving: {}",
              plain_size,
              final_size,
              saving_percent);
-}
-
-#[cfg(test)]
-mod tests {
-    use std::collections::HashMap;
-    use crate::{random_key, random_value, compress};
-
-    #[test]
-    fn same_output() {
-        let key_length: usize = 30;
-        let num_entries = 1000;
-
-        let mut original: HashMap<String, bool> = HashMap::new();
-        (0..num_entries).for_each(|_| {
-            original.insert(random_key(key_length),random_value());
-        });
-
-        let bloom_map = compress(&original);
-
-        for key in original.keys() {
-            assert_eq!(bloom_map.get(key), original.get(key));
-        }
-    }
 }
